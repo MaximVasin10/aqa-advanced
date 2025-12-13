5cc0e688b61ff1ecde48dcd8af9aa9628ba10a01
Maksym <maximvasin10@gmail.com>
2025-12-13 14:33:19 +0200

    lesson 4 - jsOperators

---
+++ lesson4_jsOperators/task4.js
@@ -0,0 +1,18 @@
+//   task 4.1
+
+let radius = 3;
+
+console.log("Circle area: " + (Math.PI * Math.pow(radius, 2)).toFixed(2));
+
+//   task 4.2
+
+let length = 5;
+let width = 4;
+
+console.log("Rectangle area: " + (length * width).toFixed(2));
+
+//    task 4.3
+
+radius = 4;
+let height = 10;
+console.log("Cylinder area: " + (Math.PI * (Math.pow(radius, 2) * height)).toFixed(2));
