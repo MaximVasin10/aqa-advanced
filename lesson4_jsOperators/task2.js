ae409739c7c9be4dd4ab95a9c5bbeaab3e91d2f7
Maksym <maximvasin10@gmail.com>
2025-12-14 14:32:25 +0200

    task2,3 - fix formatting issues

--- lesson4_jsOperators/task2.js
+++ lesson4_jsOperators/task2.js
@@ -1,9 +1,9 @@
 let str = "John";
 let str2 = "Silvia";
 
-let welcome = "Hello "
+let welcome = "Hello ";
 let welcomeJohn = welcome + str;
-let welcomeSilvia = welcome + str2
+let welcomeSilvia = welcome + str2;
 
 console.log(welcomeJohn);
 console.log(welcomeSilvia);
