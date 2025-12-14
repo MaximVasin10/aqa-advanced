ffb5182eda8d8af265f54c9f2e09efdc175973d0
Maksym <maximvasin10@gmail.com>
2025-12-14 14:38:31 +0200

    task1 - fix format

--- lesson5/task1.js
+++ lesson5/task1.js
@@ -1,16 +1,13 @@
 let averageGrade = 100;
 
-
-if(averageGrade < 60){
-    console.log("Really bad student (less than 60)");
-}else if(averageGrade >=60 && averageGrade <=70){
-    console.log("Normal student (60-70)");
-}else if(averageGrade >70 && averageGrade <=80){
-    console.log("Good student (70-80)");
-}
-else if(averageGrade >80 && averageGrade <=90){
-    console.log("Excellent user (80-90)");
-}
-else if(averageGrade >90 && averageGrade <=100){
-    console.log("You will get the scholarship, congrats! (90-100)");
+if (averageGrade < 60) {
+  console.log("Really bad student (less than 60)");
+} else if (averageGrade >= 60 && averageGrade <= 70) {
+  console.log("Normal student (60-70)");
+} else if (averageGrade > 70 && averageGrade <= 80) {
+  console.log("Good student (70-80)");
+} else if (averageGrade > 80 && averageGrade <= 90) {
+  console.log("Excellent user (80-90)");
+} else if (averageGrade > 90 && averageGrade <= 100) {
+  console.log("You will get the scholarship, congrats! (90-100)");
 }
